b7490c67cdec2cc478d482e214c942998990846d
Guillermo Velázquez <gago28@gmail.com>
2022-12-24 22:03:25 -0300

    Agregado contenido final

--- js/main.js
+++ js/main.js
@@ -1,37 +1,21 @@
-// const navToggleLabel = document.querySelector(".nav-toggle-label");
-
-// let menuOpen = false;
-
-// navToggleLabel.addEventListener('click', () => {
-//     if (!menuOpen) {
-//     navToggleLabel.classList.add('open');
-//     menuOpen = true;
-//     } else {
-//         navToggleLabel.classList.remove('open');
-//         menuOpen = false;
-//     }
-// })
-
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
     speed:800,
-    autoplay:{delay: 5000},
-  
+    autoplay:{delay: 3500},
+ 
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
   
     // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
   });
+
+  swiper.click(swiper, navigationNext)
